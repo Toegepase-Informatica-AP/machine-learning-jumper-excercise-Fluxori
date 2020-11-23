8a55cdb5a71d62d464b1df8e57f6351ef42cbacf
Anne Toussaint <30604048+Fluxori@users.noreply.github.com>
2020-11-23 10:56:19 +0100

    Fix problem with heuristic

--- jumper/Assets/Scripts/Environment.cs
+++ jumper/Assets/Scripts/Environment.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
 public class Environment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Obstacle obstaclePrefab;
+    
+    private Jumper jumper;
+    private TextMeshPro scoreBoard;
+    private GameObject obstacles;
+
+    public void OnEnable()
     {
-        
+        obstacles = transform.Find("Obstacles").gameObject;
+        scoreBoard = transform.GetComponentInChildren<TextMeshPro>();
+        jumper = transform.GetComponentInChildren<Jumper>();        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        scoreBoard.text = jumper.GetCumulativeReward().ToString("f2");
+    }
+
+    public void SpawnObstacle()
+    {
+        GameObject obstacle = Instantiate(obstaclePrefab.gameObject);
+        obstacle.transform.SetParent(obstacles.transform);
+        obstacle.transform.localPosition = obstacles.transform.localPosition;
+    }
+
+    // public Vector3 RandomPosition(float up)
+    // {
+    //     float x = Random.Range(-9.75f, 9.75f);
+    //     float z = Random.Range(-9.75f, 9.75f);
+    //
+    //     return new Vector3(x, up, z);
+    // }
+
+    public void ClearEnvironment()
+    {
+        foreach (Transform obstacle in obstacles.transform)
+        {
+            GameObject.Destroy(obstacle.gameObject);
+        }
     }
 }
---
+++ jumper/Assets/Scripts/Jumper.cs
@@ -0,0 +1,80 @@
+﻿using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class Jumper : Agent
+{
+    public float speed = 10;
+    public float jumpForce = 2.0f;
+
+    private Rigidbody body;
+    private Environment environment;
+    private int menhirCount;
+    private bool isOnGround = true;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        body = GetComponent<Rigidbody>();
+        environment = GetComponentInParent<Environment>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // transform.localPosition = new Vector3(0f, 1.5f, 0f);
+        // transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        body.angularVelocity = Vector3.zero;
+        body.velocity = Vector3.zero;
+        environment.ClearEnvironment();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(isOnGround);
+
+        // TODO: zien of we hier een reward moeten zetten (0.1 om te blijven "leven")
+        // if (transform.localPosition.y < 0)
+        // {
+        //     AddReward(-1f);
+        //     EndEpisode();
+        // }
+    }
+    
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0f;
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            actionsOut[0] = 1f;
+        }
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (vectorAction[0] == 1 && isOnGround)
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        body.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        isOnGround = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        if (other.transform.CompareTag("Obstacle"))
+        {
+            AddReward(-1f);
+            
+            EndEpisode();
+        }
+    }
+}
